c439b69884d7f134cae7d6865a23923b43cd3996
helpdeskng <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>
2022-04-08 15:25:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - The program starts from the main function
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+char ch;
+for(ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
