4006080f083573e352b9ef6eec5d393a249487e5
helpdeskng <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>
2022-04-08 15:12:38 -0700

    Small fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, x);
 }
 else if (x == 0)
 {
-printf("Last digit of %d is %d is 0\n", n, x);
+printf("Last digit of %d is %d and is 0\n", n, x);
 }
 else
 {
