4addb16fd17551979d9837f4f80c41981cef400b
helpdeskng <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>
2022-04-09 13:50:49 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,6 +11,5 @@ for (numbers = 0; numbers <= 9; numbers++)
 {
 putchar(numbers);
 }
-putchar('\n');
 return (0);
 }
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This progrm would display lowercase alphabetsin reverse order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char alphabets;
+  for (alphabets = 'z'; alphabets >= 'a'; alphabets--){
+    putchar(alphabets);
+  }
+  putchar('\n');
+  return (0);
+}
