102c1dc1f00144cf9d287302610114ee833bb373
helpdeskng <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>
2022-04-09 14:32:30 -0700

    Fixed 5 to while loop

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,12 @@
 int main(void)
 {
 int number;
-for (number = 0; number <= 9; number++)
+number = 0;
+while (number <= 9)
+   /*for (number = 0; number <= 9; number++)*/
 {
 printf("%d", number);
+number++;
 }
 printf("\n");
 return (0);
