8554e47f3a41cec171ba9360dbc9f41c24de86ae
helpdeskng <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>
2022-04-09 14:23:22 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,12 @@
 */
 int main(void)
 {
-int numbers;
-for (numbers = 0; numbers <= 9; numbers++)
+char numbers = '0';
+while (numbers <= '9')
 {
 putchar(numbers);
+numbers++;
 }
+putchar('\n');
 return (0);
 }
