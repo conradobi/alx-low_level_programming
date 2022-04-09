e8e458c037d29fb81cf047467e332aa63cb328a4
helpdeskng <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>
2022-04-09 13:55:32 -0700

    Fix Betty

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * This progrm would display lowercase alphabetsin reverse order
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* This progrm would display lowercase alphabetsin reverse order
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  char alphabets;
-  for (alphabets = 'z'; alphabets >= 'a'; alphabets--){
-    putchar(alphabets);
-  }
-  putchar('\n');
-  return (0);
+char alphabets;
+for (alphabets = 'z'; alphabets >= 'a'; alphabets--)
+{
+putchar(alphabets);
+}
+putchar('\n');
+return (0);
 }
