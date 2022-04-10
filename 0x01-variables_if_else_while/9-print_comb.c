258149262db8cd22d1ea027df2e9033ea3d6594c
helpdeskng <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>
2022-04-09 18:48:57 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numSet;
+numSet = 0;
+while (numSet < 10)
+{
+putchar(48 + numSet);
+if (numSet != 9)
+{
+putchar(',');
+putchar(' ');
+}
+numSet++;
+}
+putchar('\n');
+return (0);
+}
