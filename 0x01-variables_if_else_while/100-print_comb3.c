aaaab13a22a97c7c25b15992ce8d30f81c4de7d6
helpdeskng <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>
2022-04-10 12:59:33 -0700

    Small bug fix > 100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,27 +16,28 @@
 int main(void)
 {
 int x, y;
- x = 48;
- y = 48;
- while (y < 58)
-   {
-     x = 48;
-     while (x < 58)
-       {
-	 if (y != x && y < x)
-	   {
-	     putchar(y);
-	     putchar(x);
-	     if (x == 57 && y == 56)
-	       {
-		 break;
-	       }
-	     putchar(',');
-	   }
-	 x++;
-       }
-     y++;
-   }
+x = 48;
+y = 48;
+while (y < 58)
+{
+x = 48;
+while (x < 58)
+{
+if (y != x && y < x)
+{
+putchar(y);
+putchar(x);
+if (x == 57 && y == 56)
+{
+break;
+}
+putchar(',');	     
+putchar(' ');
+}
+x++;
+}
+y++;
+}
 putchar('\n');
 return (0);
 }
