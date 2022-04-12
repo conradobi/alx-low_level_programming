06f6b365fc665a8217fb5a5e513ace7e4c6c69de
helpdeskng <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>
2022-04-12 13:57:20 -0700

    fix bug in 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Entry point
- *
- * Return: void
- */
+* print_alphabet_x10 - Entry point
+*
+* Return: void
+*/
 void print_alphabet_x10(void)
 {
 int count = 0;
-while (count < 11)
-  {
-    char alphabet = 'a';
-  while (alphabet <= 'z')
-    {
-      _putchar(alphabet);
-      alphabet++;
-    }
-  
-  count++;
- _putchar('\n');
-  }
- 
+while (count < 10)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+count++;
+_putchar('\n');
+}
 }
