20dfd3a0270d7c3c76c586fb341e6420a221111d
helpdeskng <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>
2022-04-14 16:35:39 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(voiint ch;
+for (ch = 48; ch < 58; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
