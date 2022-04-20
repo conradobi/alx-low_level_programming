7fd1e854c8e41071ca405385f15fdc0b09dcf116
helpdeskng <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>
2022-04-20 13:15:31 -0700

    small fix > 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * rest_to_98 function declearation
- *
+ * rest_to_98 - will update the value it points to
+ * @n is a pointer variable
  * Returns: Void
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
