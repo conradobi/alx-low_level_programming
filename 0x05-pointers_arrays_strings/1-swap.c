a0d848d2e9b55f48e6e929e47b17bd98daf0360e
helpdeskng <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>
2022-04-20 13:41:43 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values in a and b
+ *@a: pointer for the address of a
+ *@b: pointer for the address of b
+ *Return: 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
