502f3b7c818e30d42eff1ff67a77ccd5520f9694
helpdeskng <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>
2022-04-23 13:05:58 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+
+  int i;
+  int j;
+
+  a = 0;
+
+  for(i = 0; dest[i] != 0; i++)
+    {
+
+    }
+  for(j = 0; src[j] != 0; i++; j++)
+    {
+
+    }
+  return(dest);
+
+}
