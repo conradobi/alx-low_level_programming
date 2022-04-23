81f715503a27d920fe032394d674932d65c18a61
helpdeskng <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>
2022-04-23 13:19:39 -0700

    Bug fix > 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
-for(i = 0; dest[i] != 0; i++)
+for (i = 0; dest[i] != 0; i++)
 {
 }
 j = 0;
-while(src[j] != 0)
+while (src[j] != 0)
 {
 i++;
 j++;
 }
-return(dest);
+return (dest);
 }
