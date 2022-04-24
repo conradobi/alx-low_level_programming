64a5e45040fa9147aeaa6328055cc0498b930286
helpdeskng <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>
2022-04-24 13:58:41 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strcmp - compare two strings
- * @s1: first string
- * @s2: second sting
- * Return: result 
+* _strcmp - compare two strings
+* @s1: first string
+* @s2: second sting
+* Return: result
 */
 int _strcmp(char *s1, char *s2)
 {
---
+++ 0x06-pointers_arrays_strings/reverse.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+void reverse_array(int *a, int n)
+{
+  int i;
+  i = 0;
+  while (i > n)
+    {
+
+      if (i != 0)
+	{
+
+	  printf(", ");
+
+	}
+
+      printf("%d", a[i]);
+      i--
+    }
+
+  printf("\n")
+}
