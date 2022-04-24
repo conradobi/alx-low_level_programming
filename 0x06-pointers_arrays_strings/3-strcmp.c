78810313038dc56ff86c176902f7fc5af152e069
helpdeskng <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>
2022-04-24 10:26:33 -0700

    fix 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second sting
+ */
+int _strcmp(char *s1, char *s2)
+{
+int result;
+result = strcmp(s1, s2);
+return (result);
+}
+
