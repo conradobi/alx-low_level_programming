9d112862031c932b34729be386441ef7ecc509e9
helpdeskng <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>
2022-04-29 12:16:14 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: pointer the string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+intxn = 0;
+if (*s > '\0')
+{
+x += _strlen_recursion(s + 1) + 1;
+}
+return (n);
+}
