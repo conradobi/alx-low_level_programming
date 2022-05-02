620066eb0d79c818b2d2aa9d8da0ada6267ea652
helpdeskng <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>
2022-05-02 13:04:55 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - display the number of arguments passed into the program
+ * @argc: int
+ * @argv: string lists
+ * Return: 0
+ */
 int main(int argc, char* argv[])
 {
-  (void)argv;
-  argc = argc - 1;
-  printf("%d\n", argc);  
-  return (0);
+(void)argv;
+argc = argc - 1;
+printf("%d\n", argc);  
+return (0);
 }
