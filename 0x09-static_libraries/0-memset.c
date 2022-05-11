07c6f526b749157c6bc899aabafeb929cd804fbd
helpdeskng <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>
2022-05-11 13:42:28 -0700

    update all

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * *_memset - fills memory with a constant byte.
  * @s: pointer to put the constant
@@ -6,15 +7,16 @@
  * @n: max bytes to use
  * Return: s
  */
+
+
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
-for(i = 0; i < n; i++)
-s[i] = b;
-unsigned int x = 0;
-for (; n > 0 ; x++, n--)
+
+for (i = 0; n > 0 ; i++, n--)
 {
-s[x] = b;
+	s[i] = b;
 }
+
 return (s);
 }
