75977cb906099a4ea39a634abea72c6f3c4aa6e9
helpdeskng <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>
2022-05-26 15:35:52 -0700

    small bug fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,17 @@
 #include "lists.h"
-
+/**
+ * print_listint - print elements of linkedlist
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
-  size_t new_node = 0;
-  while(h != NULL)
-    {
-      h = h->next;
-      new_node++;
-    }
-  return (new_node);
+size_t new_node = 0;
+while (h != NULL)
+{
+h = h->next;
+new_node++;
+}
+return (new_node);
 }
