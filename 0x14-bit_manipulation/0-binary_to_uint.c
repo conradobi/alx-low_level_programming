74c26f550cb511971207e36e1169594e7025691e
helpdeskng <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>
2022-05-30 12:37:12 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int k = 1;
+unsigned int i = 0;
+unsigned int len;
+int c;
+len = strlen(b);
+for (c = len - 1; c >= 0; c--)
+{
+if (b[c] != '0' && b[c] != '1')
+{
+return (0);
+}
+if (b[c] == '1')
+{
+i += k;
+}
+k *= 2;
+}
+return (i);
+}
